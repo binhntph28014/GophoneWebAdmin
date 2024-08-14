7bfeb37be1a2c02787842407e0e15fa73b404e54
binhntph28014 <binhntph28014@fpt.edu.vn>
2024-08-14 23:50:34 +0700

    Add ChangePassword

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -4,12 +4,18 @@ import {
   Layout,
   Button,
   theme,
+  Form,
+  Modal,
+  Input,
+  notification,
+  Flex,
   Dropdown,
 } from "antd";
 const { Header } = Layout;
 import { UserIcon, BellIcon, Cog6ToothIcon } from "@heroicons/react/24/solid";
 import { useDispatch, useSelector } from "react-redux";
-
+import axios from "axios";
+import Cookies from "js-cookie";
 
 const HeaderBar = ({ toggleMenu, collapsed }) => {
   const [count, setCount] = useState(0);
@@ -97,5 +103,119 @@ const HeaderBar = ({ toggleMenu, collapsed }) => {
 
 const DialogChangeProfile = ({}) => {};
 
-const DialogChangePassword = ({}) => {};
+const DialogChangePassword = ({ visible, onCancel, data }) => {
+  const [form] = Form.useForm();
+  const token = Cookies.get("token");
+  const handleFinish = (value) => {
+    const { oldPassword, newPassword, confirmPassword } = value;
+    axios
+      .put(
+        `${import.meta.env.VITE_BASE_URL}user/change-password/${
+          data?.data._id
+        }`,
+        { oldPassword, newPassword },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((response) => {
+        notification.success({
+          message: "Thành công",
+          description: "Cập nhật mật khẩu thành công!",
+          duration: 3,
+          type: "success",
+        });
+        form.resetFields();
+        onCancel();
+      })
+      .catch((error) => {
+        notification.error({
+          error: "Thất Bại",
+          description: error.data ? error.data.message : error.message,
+          duration: 3,
+          type: "error",
+        });
+      });
+  };
+  const handleClear = () => {
+    form.resetFields();
+    onCancel();
+  };
+
+  return (
+    <Modal open={visible} footer={null} onCancel={onCancel} closeIcon={false}>
+      <Flex vertical>
+        <p className="text-xl font-bold self-center my-5">Change Password</p>
+        <Form form={form} layout="vertical" onFinish={handleFinish}>
+          <Form.Item
+            label="Mật khẩu cũ"
+            name={"oldPassword"}
+            rules={[
+              { required: true, message: "Hãy nhập mật khẩu cũ của bạn" },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="Mật khẩu mới"
+            name={"newPassword"}
+            rules={[
+              { required: true, message: "hãy nhập mật khẩu mới của bạn" },
+              {
+                min: 4,
+                message: "Mật khẩu mới phải có ít nhât 8 ký tự",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="Xác nhận mật khẩu"
+            name={"confirmPassword"}
+            rules={[
+              {
+                required: true,
+                message: "Nhập lại mật khẩu",
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("newPassword") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error("Mật khẩu không khớp"));
+                },
+              }),
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <div className="flex flex-row items-center justify-between">
+            <Form.Item>
+              <Button
+                htmlType="button"
+                className="w-[230px]"
+                onClick={handleClear}
+              >
+                Clear
+              </Button>
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                htmlType="submit"
+                type="primary"
+                className="bg-[#407cff] px-10 w-[230px]"
+              >
+                Submit
+              </Button>
+            </Form.Item>
+          </div>
+        </Form>
+      </Flex>
+    </Modal>
+  );
+};
 export default HeaderBar;
