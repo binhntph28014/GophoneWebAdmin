0a5299b31d9745878f9b8f7c53ad07807e917629
binhntph28014 <binhntph28014@fpt.edu.vn>
2024-08-15 00:03:13 +0700

    Add ChangeProfile

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -10,12 +10,18 @@ import {
   notification,
   Flex,
   Dropdown,
+  DatePicker,
+  Space,
+  Typography,
+  Badge,
 } from "antd";
 const { Header } = Layout;
 import { UserIcon, BellIcon, Cog6ToothIcon } from "@heroicons/react/24/solid";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import Cookies from "js-cookie";
+import { fetchMyProfileRequest } from "./../redux/actions/MyProfile";
+import moment from "moment";
 
 const HeaderBar = ({ toggleMenu, collapsed }) => {
   const [count, setCount] = useState(0);
@@ -101,7 +107,126 @@ const HeaderBar = ({ toggleMenu, collapsed }) => {
   );
 };
 
-const DialogChangeProfile = ({}) => {};
+const DialogChangeProfile = ({ visible, data, onCancel }) => {
+  const [form] = Form.useForm();
+  const dispatch = useDispatch();
+  const dateFormat = "DD/MM/YYYY";
+  const token = Cookies.get("token");
+
+  const handleFinish = (values) => {
+    console.log("Form values:", values);
+
+    const { username, birthday, full_name } = values;
+
+    const formattedBirthday = moment(birthday);
+
+    axios
+      .put(
+        `${import.meta.env.VITE_BASE_URL}user/edit-profile/${data?.data._id}`,
+        {
+          username: username,
+          full_name: full_name,
+          birthday: formattedBirthday,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((response) => {
+        dispatch(fetchMyProfileRequest(data?.data._id, token));
+        notification.success({
+          message: "Thành công",
+          description: "Cập nhật thông tin cá nhân thành công!",
+          duration: 3,
+          type: "success",
+        });
+        form.resetFields();
+        onCancel();
+      })
+      .catch((error) => {
+        console.log(error);
+        notification.error({
+          error: "Thất Bại",
+          description: "Cập nhật thông tin cá nhân thất bại!",
+          duration: 3,
+          type: "error",
+        });
+      });
+  };
+
+  return (
+    <Modal open={visible} footer={null} onCancel={onCancel}>
+      <Flex className="bg-white" vertical>
+        <p className="text-xl font-bold self-center my-5">
+          Chỉnh sửa thông tin cá nhân
+        </p>
+        <Form
+          form={form}
+          layout="vertical"
+          size="middle"
+          onFinish={handleFinish}
+        >
+          <Form.Item
+            name="email"
+            label="Email"
+            initialValue={data?.data?.email}
+          >
+            <Input disabled />
+          </Form.Item>
+          <Form.Item
+            name="username"
+            label="Username"
+            initialValue={data?.data?.username}
+          >
+            <Input placeholder="enter your username" />
+          </Form.Item>
+          <Form.Item
+            name="full_name"
+            label="Full Name"
+            initialValue={data?.data?.full_name}
+          >
+            <Input placeholder="enter your full name" />
+          </Form.Item>
+          <Form.Item
+            name="birthday"
+            label="Birthday"
+            initialValue={
+              data?.data?.birthday ? moment(data?.data?.birthday) : null
+            }
+          >
+            <DatePicker
+              format={dateFormat}
+              className="w-full"
+              placeholder="select birthday"
+              showToday
+              picker="date"
+            />
+          </Form.Item>
+
+          <div className="flex flex-row items-center justify-between ">
+            <Form.Item>
+              <Button htmlType="reset" className="w-[230px]">
+                Clear
+              </Button>
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                htmlType="submit"
+                type="primary"
+                className="bg-[#407cff] px-10 w-[230px]"
+              >
+                Submit
+              </Button>
+            </Form.Item>
+          </div>
+        </Form>
+      </Flex>
+    </Modal>
+  );
+};
 
 const DialogChangePassword = ({ visible, onCancel, data }) => {
   const [form] = Form.useForm();
